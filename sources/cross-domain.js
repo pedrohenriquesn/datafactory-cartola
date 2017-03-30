0449c59efbf1cb014cf75cad4c771908ffa56cea
pedrohenriquesn <pedrohsn.developer@gmail.com>
2017-03-29 23:39:23 -0300

    escudos dos clubes formato .svg

--- sources/cross-domain.js
+++ sources/cross-domain.js
@@ -1,19 +1,21 @@
 var $ = require("jquery")
+var FileSave = require('file-saver')
 
-var tabela = []
-
-module.exports = function (name, element) {
-    $(".btn-get").click(function(){ buscaDados() });
+module.exports = function () {
+    buscaDados()
 };
 
 function buscaDados() {
+
+    const tabela = []
+    const jogos = []
+
     $.ajax({
         type: "GET",
         url: "http://globoesporte.globo.com/futebol/brasileirao-serie-a/",
         success: function (data) {
             let page = document.createElement('DIV')
-            page.innerHTML = data
-            
+            page.innerHTML = data   
             // Monta ARRAY referente a criacao da tabela
             let times = $(page).find('.tabela-times .tabela-body-linha')
             for ( let i = 0 ; i < times.length ; i++ ) { 
@@ -22,12 +24,30 @@ function buscaDados() {
                 let posicaoTime = $(time).find('.tabela-times-posicao')[0].textContent
                 tabela.push({"clube": nomeTime, "posicao": posicaoTime})
             }
-
             // Monta ARRAY referente aos confrontos da rodada
             let confrontos = $(page).find('.lista-de-jogos-conteudo .lista-de-jogos-item')
+            for ( let i = 0 ; i < confrontos.length ; i++ ) {
+                let informacoes = $(confrontos[i]).find('.placar-jogo-informacoes')[0].textContent
+                let timeCasa = $(confrontos[i]).find('.placar-jogo-equipes-nome')[0].textContent
+                let timeFora = $(confrontos[i]).find('.placar-jogo-equipes-nome')[1].textContent
+                jogos.push({"dataAndLocal": informacoes, "timeCasa": timeCasa, "timeFora": timeFora})
+            }
+            writeFile(tabela, jogos)
         },
         error: function() { 
             console.log('Fudeu deu erro!');
         },
     });
+}
+
+function writeFile(classificacao, confrontos) {
+    
+    arrayClassificacao = 'classificacao = '+ JSON.stringify(classificacao)
+    arrayConfrontos = 'jogos = '+ JSON.stringify(confrontos)
+
+    var blob = new Blob([arrayClassificacao], {type: "text/plain;charset=utf-8"})
+    FileSave.saveAs(blob, 'classificacao.js')
+
+    var blob = new Blob([arrayConfrontos], {type: "text/plain;charset=utf-8"})
+    FileSave.saveAs(blob, 'confrontos.js')
 }
--- sources/main.js
+++ sources/main.js
@@ -1,7 +1,7 @@
+var $ = require("jquery")
 var cross = require('./cross-domain');
 
-var FileSave = require('file-saver')
-var blob = new Blob(["Hello, word!"], {type: "text/plain;charset=utf-8"})
-FileSave.saveAs(blob, 'teste.js');
 
-cross()
+$(".btn-get").click(function(){ 
+    cross()
+});
